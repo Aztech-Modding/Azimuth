8b6238cb65bb33df027eab45e22ab153ae4f046d
cake <cakeistastydev@gmail.com>
2026-03-06 18:08:43 +0000

    I may not know how to operate github

--- src/main/java/com/cake/azimuth/behaviour/SuperBlockEntityBehaviour.java
+++ src/main/java/com/cake/azimuth/behaviour/SuperBlockEntityBehaviour.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -273,9 +274,15 @@ public abstract class SuperBlockEntityBehaviour extends BlockEntityBehaviour {
                 ));
     }
 
+    //endregion
+
     public void onBlockBroken(final BlockEvent.BreakEvent event) {
     }
 
-    //endregion
+    public void onItemUse(final PlayerInteractEvent.RightClickBlock event) {
+    }
+
+    public void onBlockPlaced(final BlockEvent.EntityPlaceEvent event) {
+    }
 
 }
